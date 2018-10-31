e25b3b8a259246e5a7bc04e524cdb47f7b78b3e8
kthong3 <kthongsakounh@gmail.com>
2018-10-31 10:35:31 -0500

    add environment variables and route parameters

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Listening on port ${port}...`));
